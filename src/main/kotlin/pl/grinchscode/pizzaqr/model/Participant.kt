71da63672367e2587cda69c5d05e27e5072d1c03
karol <karoljurski1@gmail.com>
2019-11-13 21:28:46 +0100

    Remove participants.

--- src/main/kotlin/pl/grinchscode/pizzaqr/model/Participant.kt
+++
@@ -1,6 +0,0 @@
-package pl.grinchscode.pizzaqr.model
-
-data class Participant(val _id: String,
-                       val firstName: String,
-                       val surname: String,
-                       val tokens: List<String>)
